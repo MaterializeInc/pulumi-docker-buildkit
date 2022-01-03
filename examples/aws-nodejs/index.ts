6e35402fdd5dd26286687ceda9f3707e0a10e501
Nikhil Benesch <nikhil.benesch@gmail.com>
2022-01-02 23:38:32 -0500

    Add Node.js example

---
+++ examples/aws-nodejs/index.ts
@@ -0,0 +1,16 @@
+import * as aws from "@pulumi/aws";
+import * as dockerBuildkit from "@MaterializeInc/pulumi-docker-buildkit";
+
+const repo = new aws.ecr.Repository("repo");
+
+const registryInfo = repo.registryId.apply(async id => {
+    const credentials = await aws.ecr.getCredentials({ registryId: id });
+    const authToken = Buffer.from(credentials.authorizationToken, "base64");
+    const [username, password] = authToken.toString().split(":");
+    return { server: credentials.proxyEndpoint, username: username, password: password };
+});
+
+export const image = new dockerBuildkit.Image(
+    "image",
+    { name: repo.repositoryUrl, registry: registryInfo },
+).repoDigest;
